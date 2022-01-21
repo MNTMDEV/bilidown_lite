25c700e568613d26b7948c69c4d667ec62570957
MNTMDEV <mntmdev@163.com>
2022-01-21 15:48:12 +0800

    Update UI display

--- bili_down_chrome/content.js
+++ bili_down_chrome/content.js
@@ -2,24 +2,25 @@ var url_list = [];
 var vn_list = [];
 
 function get_vn(u) {
-    var pos=0;
+    var pos = 0;
     //clear query string
-    pos=u.indexOf("?");
-    if(pos!=-1){
-        u=u.substr(0,pos);
+    pos = u.indexOf("?");
+    if (pos != -1) {
+        u = u.substr(0, pos);
     }
     //clear prefix https://???/???/ remain xxx.m4s
-    pos=u.lastIndexOf("/");
-    if(pos!=-1){
-        u=u.substr(pos+1);
+    pos = u.lastIndexOf("/");
+    if (pos != -1) {
+        u = u.substr(pos + 1);
     }
     return u;
 }
 
-function do_ins(tot_html){
+function do_ins(tot_html) {
     $('#dl-disp').remove();
-    var ins=$('<div id="dl-disp"></div>');
+    var ins = $('<div id="dl-disp"></div>');
     ins.append("<br /><p>本喵已经资瓷直接点击下载以下链接(它们包含了视频和音频):</p>");
+    ins.append("<p>如果下载速度较慢，可以复制本页面的URL和资源URL，使用多线程下载工具<a href='https://github.com/MNTMDEV/bilidown_py'>bilidown_py</a>下载呢！</p>");
     ins.append(tot_html);
     $('#viewbox_report').after(ins);
     $('#player_module').after(ins);
@@ -34,7 +35,7 @@ chrome.runtime.onMessage.addListener(
             var vn = get_vn(url);
             if (vn_list.indexOf(vn) == -1) {
                 //apend to web
-                crx_log('Accepted a link:' + url+'\r\n'+"Video name:"+vn);
+                crx_log('Accepted a link:' + url + '\r\n' + "Video name:" + vn);
                 url_list.push(url);
                 vn_list.push(vn);
             }
@@ -43,28 +44,30 @@ chrome.runtime.onMessage.addListener(
             //controll
             var tot_html = "<ul>";
             for (var i = 0; i < url_list.length; i++) {
-                var remark="类型:";
-                var qual=0;
-                var pref=vn_list[i].replace(".m4s","");
-                var pos=pref.lastIndexOf("-");
-                if(pos!=-1){
-                    pref=pref.substr(pos+1);
+                var remark = "类型:";
+                var qual = 0;
+                var pref = vn_list[i].replace(".m4s", "");
+                var pos = pref.lastIndexOf("-");
+                if (pos != -1) {
+                    pref = pref.substr(pos + 1);
                 }
-                var para=parseInt(pref);
+                var para = parseInt(pref);
                 crx_log(vn_list[i]);
-                if(para < 30200){
-                    remark+="视频";
-                    qual=para-30000;
+                if (para < 30200) {
+                    remark += "视频";
+                    qual = para - 30000;
                 }
-                else{
-                    remark+="音频";
-                    qual=para-30200;
+                else {
+                    remark += "音频";
+                    qual = para - 30200;
                 }
-                remark+=";质量:";
-                remark+=qual;
-                var link_html = "<a href='" + url_list[i] + "'>下载资源" + (i + 1) + "("+remark+")</a>";
+                remark += ";质量:";
+                remark += qual;
+                var line = "资源" + (i + 1) + "(" + remark + ") ";
+                var down_link = "<a href='" + url_list[i] + "'>下载资源</a> ";
                 tot_html += "<li>";
-                tot_html += link_html;
+                tot_html += line;
+                tot_html += down_link;
                 tot_html += "</li>";
             }
             tot_html += "</ul>";
